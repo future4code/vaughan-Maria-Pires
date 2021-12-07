5090d64ad08cf8b68746170f180970302ca091aa
mdudapires <90624608+mdudapires@users.noreply.github.com>
2021-12-06 23:17:53 -0300

    exercício do dia (completo)

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -19,7 +19,7 @@ b) Porque false é o valor de backender e a altura não foi declarada.
 */
 
 //EXERCÍCIOS DE ESCRITA DE CÓDIGO
-//1A QUESTÃO (LETRAS "A" e "B")
+//1A QUESTÃO (LETRA "A")
 const nomesPessoa = {
     nome: "Eduarda",
     apelidos: ["Duda", "Du", "Dudinha"],
@@ -28,7 +28,71 @@ apresentar = function(nomesPessoa){
     console.log(`Eu sou ${nomesPessoa.nome}, mas pode me chamar de ${nomesPessoa.apelidos[0]}, ${nomesPessoa.apelidos[1]} ou ${nomesPessoa.apelidos[2]}.`)
 }
 
-apresentaçao = apresentar(nomesPessoa)
+apresentacao = apresentar(nomesPessoa)
 
+//1A QUESTÃO (LETRA "B")
 
+const nomesNovo = {
+    ...nomesPessoa,
+    apelidos: ["Edu", "Madu", "Dupi"]
 }
+
+apresentacaoNova = apresentar(nomesNovo)
+
+//2A QUESTÃO
+//LETRA A
+
+const objeto1 = {
+    nome: "Marcela",
+    idade: 24,
+    profissao: "dentista" 
+}
+
+const objeto2 = {
+    nome: "João",
+    idade: 33,
+    profissao: "marceneiro"
+}
+
+//LETRA B
+
+dados = function(objeto1){
+    console.log(`Nome: ${objeto1.nome}`)
+    console.log(`Número de caracteres do nome: ${objeto1.nome.length}`)
+    console.log(`Idade: ${objeto1.idade}`)
+    console.log(`Profissão: ${objeto1.profissao}`)
+    console.log(`Número de caracteres da profissão: ${objeto1.profissao.length}`)
+}
+
+dados1 = dados(objeto1)
+dados2 = dados(objeto2)
+
+//3A QUESTÃO
+// LETRA "A"
+carrinho = []
+
+// LETRA "B"
+const fruta1 = {
+    nome: "melancia",
+    disponibilidade: true
+}
+
+const fruta2 = {
+    nome: "manga",
+    disponibilidade: true
+}
+
+const fruta3 = {
+    nome: "pêra",
+    disponibilidade: true
+}
+
+recebeFruta = function(fruta1){
+    carrinho.push(fruta1)
+}
+
+melancia = recebeFruta(fruta1)
+manga = recebeFruta(fruta2)
+pera = recebeFruta(fruta3)
+
+console.log(carrinho)
