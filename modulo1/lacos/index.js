c0aa01827568ae266adc62d4ea6204eed1335b7d
mdudapires <90624608+mdudapires@users.noreply.github.com>
2021-12-08 21:58:55 -0300

    atividade do dia

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -5,6 +5,15 @@ soma cada um desses valores gerados nos laços. O valor impresso é 10, que é j
 gerados em cada loop até o "i" chegar a 4.
 
 2A QUESTÃO
+a) Todos os valores acima de 18.
+b) Sim, adicionando uma variável pro índice no for e chamando ela no console.
+
+3A QUESTÃO
+*
+**
+***
+****
+
 
 
 */
@@ -26,3 +35,43 @@ if (numeroBichinhos === 0){
 
 
 //2A QUESTÃO
+//LETRA "A"
+arrayOriginal = [2, 4, 25, 29, 31, 98, 112, 174]
+
+for(let i = 0; i < arrayOriginal.length; i++){
+    console.log(arrayOriginal[i])
+}
+
+//LETRA "B"
+for(let i = 0; i < arrayOriginal.length; i++){
+    arrayOriginal[i]/10
+    console.log(arrayOriginal[i]/10)
+}
+
+//LETRA "C"
+arrayPares = []
+for(let i = 0; i < arrayOriginal.length; i++)
+if(arrayOriginal[i] % 2 == 0){
+arrayPares.push(arrayOriginal[i])
+}
+console.log(arrayPares)
+
+//LETRA "D"
+// Não entendi a pergunta :(
+
+
+//LETRA "E"
+
+let valorMaximo = 0
+for(let valor of arrayOriginal){
+    if(valor > valorMaximo){
+        valorMaximo = valor
+    }
+}
+console.log(`Valor máximo:`, valorMaximo)
+
+valorMinimo = Math.min(...arrayOriginal)
+console.log(`Valor mínimo: ${valorMinimo}`)
+
+
+
