a67cb67f1a6a6e57ed51188862f5bcd6524f476f
mdudapires <90624608+mdudapires@users.noreply.github.com>
2021-12-09 17:01:40 -0300

    exercício do dia (fazendo)

---
+++ modulo1/callbacks/index.js
@@ -0,0 +1,12 @@
+/*EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+1A QUESTÃO
+Vai imprimir, por vez, os itens dentro do objeto (nome e apelido) e o índice
+desses elementos, juntamente com o length do array.
+
+
+2A QUESTÃO
+
+*/
+
+
+
