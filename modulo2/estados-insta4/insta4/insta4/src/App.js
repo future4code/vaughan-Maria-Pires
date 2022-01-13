2e7e73157b1306455a48a8d5cbaadfe6d7b41ace
mdudapires <90624608+mdudapires@users.noreply.github.com>
2022-01-12 22:01:05 -0300

    estados-insta4

---
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+        nomeUsuario={'duda'}
+        fotoUsuario={'https://picsum.photos/50/52'}
+        fotoPost={'https://picsum.photos/200/152'}
+        />
+
+        <Post
+        nomeUsuario={'marcela'}
+        fotoUsuario={'https://picsum.photos/50/54'}
+        fotoPost={'https://picsum.photos/200/154'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
