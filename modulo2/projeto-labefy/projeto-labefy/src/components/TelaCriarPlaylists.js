c0c58268b15a43d86a3b4a7b5fc7c6b1647d12e0
mdudapires <90624608+mdudapires@users.noreply.github.com>
2022-01-28 16:46:57 -0300

    novas alterações

--- modulo2/projeto-labefy/projeto-labefy/src/components/TelaCriarPlaylists.js
+++ modulo2/projeto-labefy/projeto-labefy/src/components/TelaCriarPlaylists.js
@@ -2,13 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import axios from "axios";
 
+
 export default class TelaCriarPlaylists extends React.Component{
     state = {
-        nome: ""
+        nome: "",
+        musica: ""
     }
 
     onChangeNome = (event) => {
         this.setState({nome: event.target.value})
+    }
+
+    onChangeMusica = (event) => {
+        this.setState({musica: event.target.value})
     }
 
 
@@ -21,11 +27,30 @@ export default class TelaCriarPlaylists extends React.Component{
         .post(url, body, axiosConfig)
         .then((response) => {
             alert("Playlist criada com sucesso!")
-            this.getAllUsers()
         })
         .catch((error) => {
             alert("Ocorreu um erro. Tente novamente.")
         })
+    }
+
+    adicionaMusica = () => {
+        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/:playlistId/tracks"
+        const body =  {
+            "name": "", 
+            "artist": "",
+            "url": ""
+        }
+        const axiosConfig = {headers: {Authorization: "maria-pires-vaughan"}}
+
+        axios
+        .post(url, body, axiosConfig)
+        .then((response) => {
+            console.log("Funcionou")
+        })
+        .catch((response) => {
+            console.log("Deu pau")
+        })
+
     }
 
     render(){
@@ -36,6 +61,9 @@ export default class TelaCriarPlaylists extends React.Component{
                 <h1>Criar playlists</h1>
                 <input placeholder="Dê um nome a sua playlist" value={this.state.nome} onChange={this.onChangeNome}/>
                 <button onClick={this.criaPlaylist}>Adicionar</button>
+                <hr/>
+                <input placeholder="Adicionar música" value={this.state.musica} onChange={this.onChangeMusica}/>
+                <button onClick={this.adicionaMusica}>Adicionar música</button>
             </div>
         )
     }
--- modulo2/projeto-labefy/projeto-labefy/src/components/VisualizarPlaylists.js
+++ modulo2/projeto-labefy/projeto-labefy/src/components/VisualizarPlaylists.js
@@ -1,39 +1,64 @@
 import React from "react";
-import styled from "styled-components";
 import axios from "axios";
 
 export default class VisualizarPlaylists extends React.Component{
-    state = {
-        playlists: []
-    }
-    
-    componentDidMount() {
-        this.pegarPlaylists()
-    }
 
-    pegarPlaylists = () => {
-        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
-        const axiosConfig = {headers: {Authorization: "maria-pires-vaughan"}}
-
-        axios
-        .get(url, axiosConfig)
-        .then((response) => {
-            this.setState({playlists: response.data})
-           
-        })
-        .catch((error) => {
-            alert("Ocorreu um erro. Tente novamente mais tarde.")
-        })
-
-    }
-    render(){
-        return(
-            <div>
-                <button onClick={this.props.CriarPlaylists}>Criar um nova playlist</button>
-                <button onClick={this.props.voltarHome}>Voltar para a página inicial</button>
-                <h1>Visualizar as playlists</h1>
-                {this.pegarPlaylists()}
-            </div>
-        )
-    }
-}
+            state = {
+                playlists: []
+            }
+        
+            componentDidMount() {
+                this.pegarPlaylists()
+            }
+              
+            pegarPlaylists = () => {
+                const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
+                const axiosConfig = {headers:{Authorization:"maria-pires-vaughan"}}
+        
+                axios.get(url, axiosConfig)
+                .then((response) => {
+                    this.setState({playlists: response.data.result.list})
+        
+                })
+                .catch((error) => {
+                    console.log("Ocorreu um erro. Tente novamente mais tarde.")
+                })
+        
+            }
+        
+            deletarPlaylists = (id) => {
+                const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
+                const axiosConfig = {headers: {Authorization: "maria-pires-vaughan"}}
+        
+                axios
+                .delete(url, axiosConfig)
+                .then((response) => {
+                    this.pegarPlaylists()
+        
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            }
+            render(){
+                const conjuntoDePlaylists = this.state.playlists.map((pls) => {
+                    return <div>
+                    <ul>
+                    <li>
+                    {pls.name}
+                    <button onClick={() => this.deletarPlaylists(pls.id)}>X</button>
+                    </li>
+                    </ul>
+                    </div>
+                })
+                return(
+                    <div>
+                        <button onClick={this.props.voltarHome}>Voltar para a página inicial</button>
+                        <button onClick={this.props.CriarPlaylists}>Criar uma nova playlist</button>
+                        <h1>Minhas playlists</h1>
+                        {conjuntoDePlaylists}
+                        <hr/>
+                    </div>
+                )
+            }
+        }
