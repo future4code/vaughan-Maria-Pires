d0790365f0308317b0e9fa348b72f55f4a70d29e
mdudapires <90624608+mdudapires@users.noreply.github.com>
2022-03-24 22:38:12 -0300

    aprofundamento typescript

---
+++ modulo4/aprofundamento-typescript/src/exercicio3.ts
@@ -0,0 +1,40 @@
+//3A
+type post = {
+    autor: string,
+    texto: string
+}
+
+const posts: post[] = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+  console.log(posts)
+
+  //3B
+  function buscarPostsPorAutor(posts: post[], autorInformado: string) {
+    return posts.filter(
+      (post) => {
+        return post.autor === autorInformado
+      }
+    )
+  }
+  //A entrada são os posts e a saída o autor informado.
