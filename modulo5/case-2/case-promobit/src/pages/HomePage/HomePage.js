2e0fe86cc1b75da86722e8f1d629bf6f4d497cb6
mdudapires <eduardafpires1@gmail.com>
2022-05-25 15:15:15 -0300

    implementação da paginação

--- modulo5/case-2/case-promobit/src/pages/HomePage/HomePage.js
+++ modulo5/case-2/case-promobit/src/pages/HomePage/HomePage.js
@@ -4,14 +4,17 @@ import MovieCard from "../../components/MovieCard/MovieCard";
 import { API_KEY, BASE_URL } from "../../constants/urls";
 import { ScreenContainer } from "./styled";
 
+
 const HomePage = () => {
     const [moviesList, setMoviesList] = useState([])
+    const [pageNumber, setPageNumber] = useState(1)
+    const [pages, setPages] = useState() 
 
     const getMovies = () => {
-        axios.get(`${BASE_URL}/popular?api_key=${API_KEY}`)
+        axios.get(`${BASE_URL}/popular?api_key=${API_KEY}&page=${pageNumber}`)
             .then((res) => {
-                setMoviesList(res.data.results)
-                console.log(res.data.results)
+                setMoviesList(res.data.results)   
+                setPages(res.data.total_pages)    
             })
             .catch((err) => {
                 console.log(err.message)
@@ -20,15 +23,25 @@ const HomePage = () => {
 
     useEffect(() => {
         getMovies()
-    }, [])
+    }, [getMovies()])
+
 
     const mappedMovies = !moviesList ? [] : moviesList.map((film) => {
         return <MovieCard key={film.id} movieId={film.id} title={film.title} date={film.release_date} imgSrc={`https://image.tmdb.org/t/p/w200/${film.poster_path}`}/>
       })
 
+    const arrayOfPages = Array.from(Array(pages))
+    const mappedArrayOfPages = arrayOfPages.map((p, index) => {
+        return <button value={index + 1} onClick={(event) => setPageNumber(event.target.value)}>{index + 1}</button>
+    })
+
+    console.log(pageNumber)
+
+
     return (
         <ScreenContainer>
-            {mappedMovies}        
+            {mappedMovies}   
+            {mappedArrayOfPages}
            </ScreenContainer>
     )
 }
