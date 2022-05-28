543558268546746e44ab44382f02eefc4c61c2fa
mdudapires <eduardafpires1@gmail.com>
2022-05-28 13:12:43 -0300

    novos ajustes na estilização para desktop - finalizada

--- modulo5/case-2/case-promobit/src/pages/DetailsPage/DetailsPage.js
+++ modulo5/case-2/case-promobit/src/pages/DetailsPage/DetailsPage.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { TMDBNavBar } from "../../components/Header/styled";
 import { API_KEY, BASE_URL } from "../../constants/urls";
-import { Div, H2, H2Cast, H4, OverviewText, Poster, ReleaseDate } from "./styled";
+import { Button, Div, GrayDiv, H2, H4, MovieInfos, OverviewText, Poster, Span } from "./styled";
 
 const DetailsPage = () => {
+    const navigate = useNavigate()
     const [movie, setMovie] = useState({})
     const params = useParams()
 
     const getMovieDetails = () => {
         axios.get(`${BASE_URL}/${params.id}?api_key=${API_KEY}`)
             .then((res) => {
-                console.log(res.data)
                 setMovie(res.data)
+                console.log(res.data)
             })
             .catch((err) => {
                 alert(err.message)
@@ -26,19 +27,22 @@ const DetailsPage = () => {
         getMovieDetails()
     }, [])
 
+    const genres = movie.genres
+
     return (
         <Div>
             <TMDBNavBar>
                 <Poster src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} />
             </TMDBNavBar>
             <H2>{movie.title} ({moment(movie.release_date).format("YYYY")})</H2>
-            <ReleaseDate>{moment(movie.release_date).format("DD/MM/YYYY")} (BR) | {movie.vote_average} | {movie.runtime}min | {(movie.original_language).toUpperCase()}</ReleaseDate>
+            <MovieInfos>{moment(movie.release_date).format("DD/MM/YYYY")} (BR) | {movie.vote_average} | {movie.runtime}min | 
+                 {genres && genres.map((film, index) => <Span key={film.id}>{(index ? ', ' : '') +film.name}</Span>
+
+                )}</MovieInfos>
             <H4>Sinopse</H4>
             <OverviewText>{movie.overview}</OverviewText>
-
-            
-    
-
+            <Button onClick={() => navigate(-1)}>Voltar à página inicial</Button>
+            <GrayDiv/>
 
         </Div>
     )
--- modulo5/case-2/case-promobit/src/pages/DetailsPage/styled.js
+++ modulo5/case-2/case-promobit/src/pages/DetailsPage/styled.js
@@ -18,7 +18,7 @@ top: 18%;
 left: 38%;
 `
 
-export const ReleaseDate = styled.p`
+export const MovieInfos = styled.p`
 color: white;
 position: absolute;
 top: 25%;
@@ -48,3 +48,30 @@ top: 82%;
 left: 16%;
 `
 
+export const Span = styled.span`
+margin-left: 2px;
+`
+
+export const GrayDiv = styled.div`
+background-color: #d1d1e0;
+width: 100%;
+height: 190px;
+`
+
+export const Button = styled.button`
+background-color: black;
+color: white;
+position: absolute;
+left: 38%;
+top: 55%;
+border: 1px solid black;
+width: 150px;
+height: 30px;
+border-radius: 10px;
+cursor: pointer;
+:hover{
+    background-color: white;
+    border: 1px solid white;
+    color: black;
+}
+`
