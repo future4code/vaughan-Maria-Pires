665995f6f66fdf57a7ad6a7ddf3d2583732d629a
mdudapires <eduardafpires1@gmail.com>
2022-06-08 20:14:00 -0300

    páginade detalhes com informacoes requisitadas

--- modulo5/case-4/case-sicredi/src/pages/DetailsPage/DetailsPage.js
+++ modulo5/case-4/case-sicredi/src/pages/DetailsPage/DetailsPage.js
@@ -1,9 +1,33 @@
+import axios from "axios";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { BASE_URL } from "../../constants/urls";
 
 
 const DetailsPage = () => {
+    const [dragonDetails, setDragonDetails] = useState([])
+    const params = useParams()
+
+    const getDragonsDetails = () => {
+        axios.get(`${BASE_URL}/${params.id}`)
+        .then((res) => {
+            setDragonDetails(res.data)
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+    }
+
+    useEffect(() => {
+        getDragonsDetails();
+    })
     return(
         <>
-        Página de detalhes
+        <p>Data de criação: {moment(dragonDetails.createdAt).format("DD/MM/YYYY")}</p>
+        <p>Nome: {dragonDetails.name}</p>
+        <p>Tipo: {dragonDetails.type}</p>
+
         </>
     )
 }
