346699dc16bc1005cd54fb70b8c0d8f44bc82434
mdudapires <eduardafpires1@gmail.com>
2022-06-09 22:27:31 -0300

    página de erro estilizada e responsiva

--- modulo5/case-4/case-sicredi/src/pages/ErrorPage/ErrorPage.js
+++ modulo5/case-4/case-sicredi/src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,17 @@
+import { Button, Img, ScreenContainer } from "./styled";
+import computer from "../../img/computer.png";
+import { useNavigate } from "react-router-dom";
+import { goToDragonsListPage } from "../../router/coordinator";
 
 
 const ErrorPage = () => {
+    const navigate = useNavigate()
     return(
-        <>
-        Página de erro</>
+        <ScreenContainer>
+            <h2>A página que você tentou acessar não existe :-(</h2>
+        <Img src={computer}/>
+        <Button onClick={() => goToDragonsListPage(navigate)}>Voltar para a home</Button>
+        </ScreenContainer>
     )
 }
 
