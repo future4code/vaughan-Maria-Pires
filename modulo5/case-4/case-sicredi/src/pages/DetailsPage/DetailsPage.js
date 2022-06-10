9ef28d0e895b20ce8edb7ca255911b9c8997f74b
mdudapires <eduardafpires1@gmail.com>
2022-06-09 22:06:46 -0300

    página de detalhes estilizada e responsiva

--- modulo5/case-4/case-sicredi/src/pages/DetailsPage/DetailsPage.js
+++ modulo5/case-4/case-sicredi/src/pages/DetailsPage/DetailsPage.js
@@ -1,34 +1,39 @@
 import axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { BASE_URL } from "../../constants/urls";
-
+import { Button, DetailsCard, Img, ScreenContainer } from "./styled";
+import dragaoV from "../../img/dragaoV.png"
 
 const DetailsPage = () => {
     const [dragonDetails, setDragonDetails] = useState([])
     const params = useParams()
+    const navigate = useNavigate()
 
     const getDragonsDetails = () => {
         axios.get(`${BASE_URL}/${params.id}`)
-        .then((res) => {
-            setDragonDetails(res.data)
-        })
-        .catch((err) => {
-            alert(err.message)
-        })
+            .then((res) => {
+                setDragonDetails(res.data)
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
     }
 
     useEffect(() => {
         getDragonsDetails();
     })
-    return(
-        <>
-        <p>Data de criação: {moment(dragonDetails.createdAt).format("DD/MM/YYYY")}</p>
-        <p>Nome: {dragonDetails.name}</p>
-        <p>Tipo: {dragonDetails.type}</p>
-
-        </>
+    return (
+        <ScreenContainer>
+            <DetailsCard>
+                <p><b>Data de criação</b>: {moment(dragonDetails.createdAt).format("DD/MM/YYYY")}</p>
+                <p><b>Nome</b>: {dragonDetails.name}</p>
+                <p><b>Tipo</b>: {dragonDetails.type}</p>
+                <Img src={dragaoV}/>
+                <Button onClick={() => navigate(-1)}>Voltar à lista</Button>
+            </DetailsCard>
+        </ScreenContainer>
     )
 }
 
